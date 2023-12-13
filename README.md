1d37c8b6ab8da623707cfafd3bcfde909e56787e
Kevin Wang <kevinwang@kevinwang.us>
2023-12-13 10:46:28 -0500

    add run and submit commands

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Kevin Wang's CLI tool for advent of code
 There are already, uh, 143492 different cmdline tools for Advent of Code.  
 See: https://github.com/Bogdanp/awesome-advent-of-code#tools-and-utilities
 
-However, this one is unique in that I made it. Due to this, it is bespoke for the way I do AOC, e.g. using `.in` files to store inputs.
+However, this one is unique in that I am making it.
+
+Due to this, it is bespoke for the way I do AOC, e.g. using `.in` files to store inputs.
 
 wimglenn/advent-of-code-data seems to be one of the most popular and polished tools, and I used its approach and some code for authing to AOC: https://github.com/wimglenn/advent-of-code-wim/issues/1 and other things.
+
+Usage:
+
+```
+aoc day 12
+aoc make
+aoc get-real
+```
--- aoc_cli/command_line.py
+++ aoc_cli/command_line.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import json
 import requests
+import subprocess
 
 BASE_URL = 'https://adventofcode.com'
 
@@ -39,7 +40,16 @@ def get_year_from_config_with_default():
         return 2023
     
 def get_token_from_config():
-    return get_config_data()['session_token']
+    data = get_config_data()
+    if 'session_token' not in data:
+        raise ValueError('You need to set your AOC session token. Use `aoc auth <session_token>`')
+    return data['session_token']
+
+def get_openai_key_from_config():
+    data = get_config_data()
+    if 'openai_key' not in data:
+        raise ValueError('You need to set your OpenAI key. Use `aoc openai <openai_key>`')
+    return data['openai_key']
 
 # Get and save inputs from adventofcode.com ###
 def get_real_input(year, day, token):
@@ -66,6 +76,19 @@ def get_year_and_day_with_fallbacks(args):
 def parse_html_and_get_article(html):
     # TODO: copy aocd and make this nicer with beautifulsoup
     return html.split('article')[1]
+
+def execute(cmd, inp):
+    popen = subprocess.Popen(cmd, stdin=inp, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    # return_code = popen.wait()
+    # if return_code:
+    #     raise subprocess.CalledProcessError(return_code, cmd)
+def run_python_with_input(py_path, input_path):
+    with open(input_path, 'r') as input_file:
+        for l in execute(['python', '-u', py_path], input_file):
+            print(l, end="")
 
 ###### commands ##############################################
 
@@ -105,12 +128,48 @@ def make(args):
 def start_daemon():
     pass
 
-def test():
-    pass
+def test(args):
+    year, day = get_year_and_day_with_fallbacks(args)
+    py_path = f'day{day}.py'
+    if not os.path.exists(py_path):
+        raise ValueError(f'file {py_path} does not exist. create it with `aoc make {day}`')
+    example_in_filepath = f'day{day}_example.in'
+    output = run_python_with_input(py_path, example_in_filepath)
+    print(output, end='')
+    
+def submit(args):
+    # used aocd/models.py:Puzzle._submit for inspiration
+    # It has a lot of useful bells and whistles like tracking previous submissions which is really nice. also automatically fixing year,day if day,year is given.
+    # so TODO: copy those nice features
+    year, day = get_year_and_day_with_fallbacks(args)
+    level = args.level
+    answer = args.answer
+    if level is None:
+        level = 1
+    if answer is None:
+        answer = input('enter answer: ')
+    answer = answer.strip()
+    url = f'{BASE_URL}/{year}/day/{day}/answer'
+    token = get_token_from_config()
+    fields = {"level": level, "answer": answer}
+    headers = {"Cookie": f"session={token}"}
+    response = requests.post(url, headers=headers, data=fields, allow_redirects=False)
+    response.raise_for_status()
+    if response.status_code != 200:
+        print(f"got {response.status_code} status code")
+        raise ValueError(f"HTTP {response.status_code} at {url}")
+    print(parse_html_and_get_article(response.text))
 
-def run():
-    pass
-
+def run(args):
+    year, day = get_year_and_day_with_fallbacks(args)
+    py_path = f'day{day}.py'
+    if not os.path.exists(py_path):
+        raise ValueError(f'file {py_path} does not exist. create it with `aoc make {day}`')
+    real_in_filepath = f'day{day}_real.in'
+    output = run_python_with_input(py_path, real_in_filepath)
+    print(output, end='')
+    
+    
 def submit(args):
     # used aocd/models.py:Puzzle._submit for inspiration
     # It has a lot of useful bells and whistles like tracking previous submissions which is really nice. also automatically fixing year,day if day,year is given.
@@ -140,6 +199,14 @@ def set_session_id(args):
     data['session_token'] = args.session_token
     write_config_data(data)
 
+def set_openai_key(args):
+    openai_key = args.openai_key
+    if openai_key is None:
+        openai_key = input('enter OpenAI key: ')
+    data = get_config_data()
+    data['openai_key'] = openai_key
+    write_config_data(data)
+
 def get_and_save_input(args):
     year, day = get_year_and_day_with_fallbacks(args)
     token = get_token_from_config()
@@ -156,7 +223,6 @@ def debug(args):
 def main():
     # create the top-level parser
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--foo', action='store_true', help='foo help')
     subparsers = parser.add_subparsers(help='sub-command help')
 
     parser_cd = subparsers.add_parser('cd', help='This should cd you to the AOC directory. But it does not work :(', description="This does not work :(")
@@ -174,6 +240,10 @@ def main():
     parser_auth.add_argument('session_token', help='session_token help')
     parser_auth.set_defaults(func = set_session_id)
 
+    parser_openai = subparsers.add_parser('openai', help='Set OpenAI API key')
+    parser_openai.add_argument('openai_key', nargs='?', default=None, help='openai_key help')
+    parser_openai.set_defaults(func = set_openai_key)
+
     parser_save_real = subparsers.add_parser('get-real', help='Download and save the real input for the day')
     parser_save_real.add_argument('year', nargs='?', default=None)
     parser_save_real.add_argument('day', nargs='?', default=None)
@@ -184,14 +254,18 @@ def main():
 
     # only example
     parser_test = subparsers.add_parser('test', help='test help')
+    parser_test.add_argument('year', nargs='?', default=None)
+    parser_test.add_argument('day', nargs='?', default=None)
     parser_test.set_defaults(func = test)
 
     # both example and input (if available)
     parser_run = subparsers.add_parser('run', help='run help')
+    parser_run.add_argument('year', nargs='?', default=None)
+    parser_run.add_argument('day', nargs='?', default=None)
     parser_run.set_defaults(func = run)
 
     # submit
-    parser_submit = subparsers.add_parser('submit', help='submit the answer')
+    parser_submit = subparsers.add_parser('submit', help='submit the answer: `aoc submit 1` to submit part 1 for the day')
     parser_submit.add_argument('level', nargs='?', default=None)
     parser_submit.add_argument('year', nargs='?', default=None)
     parser_submit.add_argument('day', nargs='?', default=None)
